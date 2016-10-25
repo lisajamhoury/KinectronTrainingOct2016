0af275c8f61868b15e35bad293458676da18770f
Lisa Jamhoury <lisajamhoury@gmail.com>
2016-10-25 11:30:13 -0400

    Add beach and catch examples

---
+++ examples/p5examples/beach/sketch.js
@@ -0,0 +1,44 @@
+// Run with simplehttpserver for image to load properly. http://www.andyjamesdavies.com/blog/javascript/simple-http-server-on-mac-os-x-in-seconds
+
+var myCanvas = null;
+var beach;
+var img;
+var myDiv;
+
+var processing = false;
+
+// Declare Kinectron
+var kinectron = null;
+
+function preload() {
+  beach = loadImage("images/beach.png");
+}
+
+function setup() {
+  myCanvas = createCanvas(640, 426);
+  background(255);
+
+  // Define and create an instance of kinectron
+  kinectron = new Kinectron("kinectron", { // username matches application display, default is kinectron 
+    "host": " ", // host matches application display
+    "port": "9001", // port matches application display
+    "path": "/" // path defaults to /
+  });
+
+  // Create connection between remote and application
+  kinectron.makeConnection();
+
+  // Start the greenscreen camera
+  kinectron.startKey(goToBeach);
+}
+
+function draw() {
+
+}
+
+function goToBeach(img) {
+  loadImage(img.src, function(loadedImage) {
+    image(beach, 0, 0);
+    image(loadedImage, 0, 0);
+  });
+}
